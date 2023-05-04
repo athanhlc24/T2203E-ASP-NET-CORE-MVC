8023e61770fa1646c24f616fee0ef48e92cdbf3d
Administrator <tranvanthanh060699@gmail.com>
2023-05-04 21:16:20 +0700

    crud

--- T2203E-ASP.NET(CORE)MVC/Controllers/Product1Controller.cs
+++ T2203E-ASP.NET(CORE)MVC/Controllers/Product1Controller.cs
@@ -15,8 +15,10 @@ namespace T2203E_ASP.NET_CORE_MVC.Controllers
         public async Task<IActionResult> Index()
         {
             List<Product> list = _context.Products.ToList<Product>();
+            ViewBag.product1 = list;
             return View();
         }
+
 
         // GET: ProductController1/Details/5
         public ActionResult Details(int id)
@@ -46,23 +48,26 @@ namespace T2203E_ASP.NET_CORE_MVC.Controllers
         }
 
         // GET: ProductController1/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int Id)
         {
-            return View();
+            var product1 = await _context.Products.FindAsync(Id); 
+            return View(product1);
         }
 
         // POST: ProductController1/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Product product)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
             }
-            catch
+           
             {
-                return View();
+                return View(product);
             }
         }
 
